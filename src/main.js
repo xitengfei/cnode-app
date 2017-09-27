1f0d074db4138d1d8dd2ec94e1d26dfa5f7d06b6
tengfei2015 <xitengfei@aragoncs.com>
2017-09-27 17:26:44 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,8 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueMaterial)
+
+Vue.material.registerTheme('default', {
+  primary: 'blue',
+  accent: 'red',
+  warn: 'red',
+  background: 'grey'
+})
 
 /* eslint-disable no-new */
 new Vue({
