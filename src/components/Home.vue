f25950f866925e8d6cf84ddb28712df86fc1959d
tengfei2015 <xitengfei@aragoncs.com>
2017-10-10 17:54:13 +0800

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,16 +4,54 @@
       <md-button class="md-icon-button" @click="toggleLeftSidenav">
         <md-icon>menu</md-icon>
       </md-button>
-
       <h2 class="md-title" style="flex: 1">CNode Zone</h2>
-
-      <md-button class="md-icon-button">
-        <md-icon>favorite</md-icon>
-      </md-button>
     </md-toolbar>
     
     <side-nav ref="sidenav"></side-nav>
-
+    
+    <div class="list-view topic-list">
+      <ul>
+        <li class="item">
+          <div class="header"><span>问答</span><h3>对于一个开源的基于Node.js量化交易平台有什么建议?</h3></div>
+          <div class="content">
+            <div class="userinfo">
+              <a href="/"><img src="https://avatars0.githubusercontent.com/u/1655233?v=4&s=120" alt="avatar" /></a>
+              <div class="username">似水年华</div>
+            </div>
+            <div class="metas">
+              <p><span class="reply">1</span> / <span class="view">20</span></p>
+              <p class="timeago">1小时前</p>
+            </div>
+          </div>
+        </li>
+        <li class="item">
+          <div class="header"><span>问答</span><h3>对于一个开源的基于Node.js量化交易平台有什么建议?</h3></div>
+          <div class="content">
+            <div class="userinfo">
+              <a href="/"><img src="https://avatars0.githubusercontent.com/u/1655233?v=4&s=120" alt="avatar" /></a>
+              <div class="username">似水年华</div>
+            </div>
+            <div class="metas">
+              <p><span class="reply">1</span> / <span class="view">20</span></p>
+              <p class="timeago">1小时前</p>
+            </div>
+          </div>
+        </li>
+        <li class="item">
+          <div class="header"><span>问答</span><h3>对于一个开源的基于Node.js量化交易平台有什么建议?</h3></div>
+          <div class="content">
+            <div class="userinfo">
+              <a href="/"><img src="https://avatars0.githubusercontent.com/u/1655233?v=4&s=120" alt="avatar" /></a>
+              <div class="username">似水年华</div>
+            </div>
+            <div class="metas">
+              <p><span class="reply">1</span> / <span class="view">20</span></p>
+              <p class="timeago">1小时前</p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -32,3 +70,13 @@ export default {
   }
 }
 </script>
+
+<style type="text/css">
+  .list-view{background:#fff;}
+  .list-view li{border-bottom: 1px solid #ddd;padding: 0.2rem;}
+  .topic-list{
+    .header{display: -webkit-box;display: -ms-flexbox;display: flex;align-items: center;}
+  }
+  .topic-list .header h3{width:80%;font-size:0.32rem;line-height: 0.56rem;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;}
+  .topic-list .header span{font-size:0.24rem;margin-right:0.1rem;padding:0.01rem;background:#2196f3;border-radius:0.06rem;}
+</style>
